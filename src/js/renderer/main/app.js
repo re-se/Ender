ccb56ae915821ece2e99833eb2bc22235d467b83
ss <shire0301@gmail.com>
2018-12-30 00:04:32 +0900

    config の指定がない場合の挙動を実装

--- src/js/renderer/main/app.js
+++ src/js/renderer/main/app.js
@@ -3,17 +3,20 @@ import { ipcRenderer } from 'electron'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import path from 'path'
 
 import Game from '../components/Game'
 import engine from './engine'
-import { toAbsolutePath } from '../util/util'
+import { toAbsolutePath, toAbsoluteAppPath } from '../util/util'
 import store from './store'
 import { generateConfig } from './config'
 
+const DEFAULT_CONFIG_PATH = 'config.json'
+
 ipcRenderer.send('request-config-path')
 ipcRenderer.on('set-config-path', (e, requestConfigPath) => {
-  let configPath = null
-  if (requestConfigPath !== undefined) {
+  let configPath = toAbsoluteAppPath(DEFAULT_CONFIG_PATH)
+  if (requestConfigPath !== undefined && requestConfigPath !== null) {
     configPath = toAbsolutePath(requestConfigPath)
   }
   console.log(configPath)
